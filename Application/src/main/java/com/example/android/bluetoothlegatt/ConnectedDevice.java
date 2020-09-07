f3e257d0e03020a214942c2aae112049b14b92cf
Milos <mlscabrilo5@gmail.com>
2020-09-07 02:00:31 +0200

    Initial commit

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/ConnectedDevice.java
@@ -0,0 +1,19 @@
+package com.example.android.bluetoothlegatt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConnectedDevice extends Activity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.send_receive_page);
+
+
+    }
+
+
+
+}
